53fc67c11298dd2dc8aa764d35f0cec276fadc82
AndresBouhut <andresbouhutdocumentos@gmail.com>
2025-03-31 19:41:28 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function primerElementoArray(arr1) {
+  return arr1[0]
+}
+
+let createExcuse = ()=>{
+  let primerElementoArray = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+  let first = ["The "];
+  let who = ["dog ", "grandma ", "mailman ", "bird ", "driver "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let possetion = ["my homework ", "my phone ", "the car "];
+  let when = ["before the class", "when i was sleeping", "while i was exercising", "during my lunch", "while i was praying"];
+  let firstFirst = primerElementoArray(first)
+  let firstWho = primerElementoArray(who)
+  let firstAction = primerElementoArray(action)
+  let firstPossetion = primerElementoArray(possetion)
+  let firstWhen = primerElementoArray(when)
+  let excuse = document.getElementById("excuse")
+  excuse.innerText = firstFirst + firstWho + firstAction + firstPossetion + firstWhen ;
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  createExcuse()
+  
 };
+  
+ 
+
+
+
