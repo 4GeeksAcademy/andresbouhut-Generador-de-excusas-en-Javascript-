704c94e95c9b0d1f8762900f65b54c585fdd868c
AndresBouhut <andresbouhutdocumentos@gmail.com>
2025-04-04 18:37:17 +0000

    eliminado let The

--- src/app.js
+++ src/app.js
@@ -11,18 +11,18 @@ let createExcuse = ()=>{
   let primerElementoArray = (array) => {
     return array[Math.floor(Math.random() * array.length)];
   };
-  let first = ["The "];
+  
   let who = ["dog ", "grandma ", "mailman ", "bird ", "driver "];
   let action = ["ate ", "peed ", "crushed ", "broke "];
   let possetion = ["my homework ", "my phone ", "the car "];
   let when = ["before the class", "when i was sleeping", "while i was exercising", "during my lunch", "while i was praying"];
-  let firstFirst = primerElementoArray(first)
+  
   let firstWho = primerElementoArray(who)
   let firstAction = primerElementoArray(action)
   let firstPossetion = primerElementoArray(possetion)
   let firstWhen = primerElementoArray(when)
   let excuse = document.getElementById("excuse")
-  excuse.innerText = firstFirst + firstWho + firstAction + firstPossetion + firstWhen ;
+  excuse.innerText = "The " + firstWho + firstAction + firstPossetion + firstWhen ;
 }
 window.onload = function() {
   createExcuse()
